d86ade46d8af57bb96ba5ad5d76e961503dbd57d
Joshua Christian <jochris1506@gmail.com>
2022-09-08 20:16:02 +0700

    redis participant done

--- server/controllers/participantsController.js
+++ server/controllers/participantsController.js
@@ -1,5 +1,6 @@
 const { Participant, Team, Event } = require("../models");
 const { Op } = require("sequelize");
+const { redis } = require('../config/redis')
 
 module.exports = class participantsController {
   static async create(req, res, next) {
@@ -12,6 +13,9 @@ module.exports = class participantsController {
         defaults: { TeamId, EventId, paymentDate: new Date() },
       });
 
+      await redis.del('app:participants')
+      await redis.del('app:participantId')
+
       res.status(201).json(createResponse || created);
     } catch (error) {
       next(error);
@@ -19,11 +23,19 @@ module.exports = class participantsController {
   }
   static async fetchAll(req, res, next) {
     try {
-      const fetchResponse = await Participant.findAll({
-        include: [{ model: Team }, { model: Event }],
-      });
+      const participantsCache = await redis.get('app:participants')
 
-      res.status(200).json(fetchResponse || { message: "there is no data" });
+      if (participantsCache) {
+        const participants = JSON.parse(participantsCache)
+
+        res.status(200).json(participants || { message: "there is no data" })
+      } else {
+        const fetchResponse = await Participant.findAll({
+          include: [{ model: Team }, { model: Event }],
+        });
+
+        res.status(200).json(fetchResponse || { message: "there is no data" });
+      }
     } catch (error) {
       next(error);
     }
@@ -31,15 +43,24 @@ module.exports = class participantsController {
   static async fetchOneParticipantByEventId(req, res, next) {
     try {
       const { eventId } = req.params;
-      const fetchResponse = await Participant.findOne({
-        where: { EventId: +eventId },
-        include: [{ model: Team }, { model: Event }],
-      });
+      const lastIdCache = await redis.get("app:participantId");
 
-      if (!fetchResponse)
-        return res.status(404).json({ message: "data not found" });
+      if (eventId !== lastIdCache) {
+        const fetchResponse = await Participant.findOne({
+          where: { EventId: +eventId },
+          include: [{ model: Team }, { model: Event }],
+        });
 
-      res.status(200).json(fetchResponse);
+        if (!fetchResponse)
+          return res.status(404).json({ message: "data not found" });
+
+        await redis.set("app:participantDetailId", eventId)
+        await redis.set('app:participant', JSON.stringify(fetchResponse))
+        res.status(200).json(fetchResponse);
+      } else {
+        const participantDetail = JSON.parse(await redis.get('app:participant'))
+        res.status(200).json(participantDetail)
+      }
     } catch (error) {
       next(error);
     }
@@ -68,6 +89,9 @@ module.exports = class participantsController {
         { where: { TeamId: +teamId } }
       );
 
+      await redis.del('app:participants')
+      await redis.del('app:participantId')
+
       res.status(200).json({
         id: fetchResponse.id,
         TeamId: fetchResponse.TeamId,
@@ -95,6 +119,9 @@ module.exports = class participantsController {
       await Participant.destroy({
         where: { id: +participantId },
       });
+
+      await redis.del('app:participants')
+      await redis.del('app:participantId')
 
       res.status(200).json(
         fetchResponse
