525fea061e2435be46d93023dd16ed0b52075090
Joshua Christian <jochris1506@gmail.com>
2022-09-08 18:06:44 +0700

    Add redis to games controller

--- server/controllers/gamesController.js
+++ server/controllers/gamesController.js
@@ -1,6 +1,6 @@
 const { Game } = require("../models");
 const { Op } = require("sequelize");
-const games = require("../routers/gamesRoute");
+const { redis } = require('../config/redis')
 
 module.exports = class gamesController {
     static async create(req, res, next) {
@@ -8,6 +8,9 @@ module.exports = class gamesController {
             const { name, gameImg, youtubeUrl, gameUrl, releaseDate, developer, genre } = req.body
 
             const created = await Game.create({ name, gameImg, youtubeUrl, gameUrl, releaseDate, developer, genre })
+
+            await redis.del('app:games')
+            await redis.del('app:gameId')
 
             res.status(201).json(created)
         } catch (error) {
@@ -17,11 +20,21 @@ module.exports = class gamesController {
 
     static async fetchAll(req, res, next) {
         try {
-            const fetchResponse = await Game.findAll({
-                order: [["id", "desc"]]
-            })
+            const gamesCache = await redis.get('app:games')
 
-            res.status(200).json(fetchResponse || { message: "there is no data" })
+            if (gamesCache) {
+                const games = JSON.parse(gamesCache)
+
+                res.status(200).json(games || { message: "there is no data" })
+            } else {
+                const fetchResponse = await Game.findAll({
+                    order: [["id", "desc"]]
+                })
+
+                await redis.set('app:games', JSON.stringify(fetchResponse))
+
+                res.status(200).json(fetchResponse || { message: "there is no data" })
+            }
         } catch (error) {
             next(error)
         }
@@ -31,14 +44,24 @@ module.exports = class gamesController {
         try {
             const reg = new RegExp('^[0-9]*$')
             const gamesId = req.params.gamesId
+            const lastIdCache = await redis.get("app:gameId");
 
             if (reg.test(gamesId) == false) return res.status(404).json({ message: "game not found" });
 
-            const fetchResponse = await Game.findByPk(+gamesId)
+            if (gamesId !== lastIdCache) {
+                const fetchResponse = await Game.findByPk(+gamesId)
 
-            if (!fetchResponse) return res.status(404).json({ message: "game not found" });
+                if (!fetchResponse) return res.status(404).json({ message: "game not found" });
 
-            res.status(200).json(fetchResponse)
+                await redis.set("app:gameDetailId", gamesId)
+
+                await redis.set('app:game', JSON.stringify(fetchResponse))
+
+                res.status(200).json(fetchResponse)
+            } else {
+                const gameDetail = JSON.parse(await redis.get('app:game'))
+                res.status(200).json(gameDetail)
+            }
         } catch (error) {
             next(error)
         }
@@ -62,6 +85,9 @@ module.exports = class gamesController {
                     id: +gamesId
                 }
             })
+
+            await redis.del('app:games')
+            await redis.del('app:gameId')
 
             res.status(200).json({
                 message: `Success update game ${fetchResponse.name}`
@@ -87,6 +113,9 @@ module.exports = class gamesController {
                     id: gamesId
                 }
             })
+
+            await redis.del('app:games')
+            await redis.del('app:gameId')
 
             res.status(200).json({
                 message: `Success delete game ${fetchResponse.name}`
