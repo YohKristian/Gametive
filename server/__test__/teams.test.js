80c0cc38f9aedf2fe617e323eb3cf36eacf848b8
Yohanes Kristian Wibowo <yohkristianwibowo@gmail.com>
2022-09-08 19:38:19 +0700

    feat: team jest detail

--- server/__test__/teams.test.js
+++ server/__test__/teams.test.js
@@ -49,7 +49,7 @@ describe('POST Customers Login', () => {
 });
 
 describe('GET All Teams', () => {
-    // Dummy Favorite
+    // Dummy Team
     beforeAll(() => {
         return queryInterface.bulkInsert(
             "Teams",
@@ -61,6 +61,16 @@ describe('GET All Teams', () => {
                     MemberName2: "member2",
                     MemberName3: "member3",
                     MemberName4: "member4",
+                    createdAt: new Date(),
+                    updatedAt: new Date()
+                },
+                {
+                    name: "testingTeam",
+                    CaptainName: "customer",
+                    MemberName1: "",
+                    MemberName2: "",
+                    MemberName3: "",
+                    MemberName4: "",
                     createdAt: new Date(),
                     updatedAt: new Date()
                 },
@@ -79,14 +89,6 @@ describe('GET All Teams', () => {
         )
     })
 
-    // Reset DB
-    afterAll(() => {
-        return queryInterface.bulkDelete("Users", null, { truncate: true, cascade: true, restartIdentity: true })
-            .then(() => {
-                return queryInterface.bulkDelete("Teams", null, { truncate: true, cascade: true, restartIdentity: true })
-            });
-    })
-
     describe('Success GET All Teams - token customer', () => {
         it('should return array of object of all teams', async () => {
             const response = await request(app)
@@ -98,6 +100,55 @@ describe('GET All Teams', () => {
             expect(response.body[0]).toHaveProperty('id', expect.any(Number));
             expect(response.body[0]).toHaveProperty('name', expect.any(String));
             expect(response.body[0]).toHaveProperty('CaptainName', expect.any(String));
+        });
+    });
+
+    describe('Fail GET All Teams - no token', () => {
+        it('should return object', async () => {
+            const response = await request(app)
+                .get('/teams/all-teams')
+
+            expect(response.status).toBe(400);
+            expect(response.body).toBeInstanceOf(Object);
+            expect(response.body).toHaveProperty('code', expect.any(Number));
+            expect(response.body).toHaveProperty('message', expect.any(String));
+        });
+    });
+});
+
+describe('GET Detail Teams', () => {
+    // Reset DB
+    afterAll(() => {
+        return queryInterface.bulkDelete("Users", null, { truncate: true, cascade: true, restartIdentity: true })
+            .then(() => {
+                return queryInterface.bulkDelete("Teams", null, { truncate: true, cascade: true, restartIdentity: true })
+            });
+    })
+
+    describe('Success GET Detail Teams - token customer', () => {
+        it('should return array of object of all teams', async () => {
+            const response = await request(app)
+                .get('/teams/1')
+                .set('access_token', customer_token)
+
+            expect(response.status).toBe(200);
+            expect(response.body).toBeInstanceOf(Object);
+            expect(response.body).toHaveProperty('id', expect.any(Number));
+            expect(response.body).toHaveProperty('name', expect.any(String));
+            expect(response.body).toHaveProperty('CaptainName', expect.any(String));
+        });
+    });
+
+    describe('Fail GET Detail Teams - wrong teamId 999', () => {
+        it('should return array of object of all teams', async () => {
+            const response = await request(app)
+                .get('/teams/999')
+                .set('access_token', customer_token)
+
+            expect(response.status).toBe(404);
+            expect(response.body).toBeInstanceOf(Object);
+            expect(response.body).toHaveProperty('code', expect.any(Number));
+            expect(response.body).toHaveProperty('message', expect.any(String));
         });
     });
 });
