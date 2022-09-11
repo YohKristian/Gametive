fbf7e3c0defd85a66011047cb1a2d81257d16d7b
Yohanes Kristian Wibowo <yohkristianwibowo@gmail.com>
2022-09-11 22:05:11 +0700

    fix detail event

--- client/src/components/DetailGame.js
+++ client/src/components/DetailGame.js
@@ -5,63 +5,63 @@ import { fetchEventDetail } from "../store/actions";
 import { dateFormat, rupiahFormat } from "../helpers";
 import BracketViewer from "./BracketViewer";
 import axios from "axios";
-import { Link } from "react-router-dom";
 
 export default function DetailGame() {
-  const { id } = useParams();
-  const dispatch = useDispatch();
-  const { eventDetailReducer } = useSelector((state) => state);
-  const [detail, setDetail] = useState();
-  const [location, setLocation] = useState({
-    province: "",
-    regency: "",
-    district: "",
-  });
+	const { id } = useParams();
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	const { eventDetailReducer } = useSelector((state) => state);
+	const [detail, setDetail] = useState();
+	const [location, setLocation] = useState({
+		province: "",
+		regency: "",
+		district: "",
+	});
 
-  useEffect(() => {
-    dispatch(fetchEventDetail(id)).then((data) => {
-      return setDetail(data);
-    });
-  }, []);
+	useEffect(() => {
+		dispatch(fetchEventDetail(id)).then((data) => {
+			return setDetail(data);
+		});
+	}, []);
 
-  useEffect(() => {
-    getFullLocation();
-  }, [detail, location.province, location.regency]);
+	useEffect(() => {
+		getFullLocation();
+	}, [detail, location.province, location.regency]);
 
-  const getFullLocation = async () => {
-    try {
-      const { data: provinces } = await axios.get(
-        "https://www.emsifa.com/api-wilayah-indonesia/api/provinces.json"
-      );
-      const province = provinces.filter(
-        (province) => province.id == detail.Location.ProvinceId
-      )[0];
+	const getFullLocation = async () => {
+		try {
+			const { data: provinces } = await axios.get(
+				"https://www.emsifa.com/api-wilayah-indonesia/api/provinces.json"
+			);
+			const province = provinces.filter(
+				(province) => province.id == detail.Location.ProvinceId
+			)[0];
 
-      const { data: regencies } = await axios.get(
-        `https://www.emsifa.com/api-wilayah-indonesia/api/regencies/${province.id}.json`
-      );
-      const regency = regencies.filter(
-        (regency) => regency.id == detail.Location.RegencyId
-      )[0];
+			const { data: regencies } = await axios.get(
+				`https://www.emsifa.com/api-wilayah-indonesia/api/regencies/${province.id}.json`
+			);
+			const regency = regencies.filter(
+				(regency) => regency.id == detail.Location.RegencyId
+			)[0];
 
-      const { data: districts } = await axios.get(
-        `https://www.emsifa.com/api-wilayah-indonesia/api/districts/${regency.id}.json`
-      );
-      const district = districts.filter(
-        (district) => district.id == detail.Location.DistrictId
-      )[0];
+			const { data: districts } = await axios.get(
+				`https://www.emsifa.com/api-wilayah-indonesia/api/districts/${regency.id}.json`
+			);
+			const district = districts.filter(
+				(district) => district.id == detail.Location.DistrictId
+			)[0];
 
-      setLocation({
-        province: province.name,
-        regency: regency.name,
-        district: !district ? "" : district.name,
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  
-  const handlerOnClick = () => {
+			setLocation({
+				province: province.name,
+				regency: regency.name,
+				district: !district ? "" : district.name,
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const handlerOnClick = () => {
 		if (localStorage.access_token) {
 			// navigate.push("/") ke form register
 		} else {
@@ -69,52 +69,54 @@ export default function DetailGame() {
 		}
 	}
 
-  return detail ? (
-    <div className='detail'>
-      <div className='detail-img'>
-        <img src={detail.eventPoster} alt='' />
-      </div>
+	return detail ? (
+		<div className='detail'>
+			<div className='detail-img'>
+				<img src={detail.eventPoster} alt='' />
+			</div>
 
-      <div>
-        <h1>{detail.eventName}</h1>
-        <span className='status'>{detail.eventStatus}</span>
-        <h2>Description</h2>
-        <p>{detail.description}</p>
-        <h2>Rules</h2>
-        <p>{detail.rules}</p>
-        <div>
-          <p>Lokasi: Jakarta Selatan</p>
-          <p>Mulai: {dateFormat(detail.eventDate)}</p>
-          <p>Registration Fee: {rupiahFormat(detail.price)}</p>
-          <p>
-            Maps :{" "}
-            <a
-              href='/'
-              onClick={(event) => {
-                showInMapClicked(
-                  `${location.province} ${location.regency} ${location.district}`
-                );
-              }}
-            >
-              {location.province} - {location.regency}{" "}
-              {location.district ? `- ${location.district}` : ""}
-            </a>
-          </p>
-        </div>
-        
-        <BracketViewer state={JSON.parse(detail.Bracket)} />
-        
-        <button onClick={handlerOnClick}>
-				  Register event!
-			  </button>
-      </div>
-    </div>
-  ) : (
-    <h1>Loading</h1>
-  );
+			<div>
+				<h1>{detail.eventName}</h1>
+				<span className='status'>{detail.eventStatus}</span>
+				<h2>Description</h2>
+				<p>{detail.description}</p>
+				<h2>Rules</h2>
+				<p>{detail.rules}</p>
+				<div>
+					<p>Lokasi: Jakarta Selatan</p>
+					<p>Mulai: {dateFormat(detail.eventDate)}</p>
+					<p>Registration Fee: {rupiahFormat(detail.price)}</p>
+					<p>
+						Maps :{" "}
+						<a
+							href="#"
+							target="_blank"
+							onClick={(event) => {
+								event.preventDefault();
+								showInMapClicked(
+									`${location.province} ${location.regency} ${location.district}`
+								);
+							}}
+						>
+							{location.province} - {location.regency}{" "}
+							{location.district ? `- ${location.district}` : ""}
+						</a>
+					</p>
+				</div>
+
+				<BracketViewer state={JSON.parse(detail.Bracket)} />
+
+				<button onClick={handlerOnClick}>
+					Register event!
+				</button>
+			</div>
+		</div>
+	) : (
+		<h1>Loading</h1>
+	);
 }
 
 const showInMapClicked = (search) => {
-  // window.open("https://maps.google.com?q="+your_lat+","+your_lng );
-  window.open(`https://maps.google.com?q=${search}`);
+	// window.open("https://maps.google.com?q="+your_lat+","+your_lng );
+	window.open(`https://maps.google.com?q=${search}`);
 };
