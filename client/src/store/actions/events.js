40040e89b17516a742a0b66b77d83f76d3b8b673
exiast <fajararsyuwansyah@gmail.com>
2022-09-11 20:29:36 +0700

    fix: new endpoint for admin fetch event

--- client/src/store/actions/events.js
+++ client/src/store/actions/events.js
@@ -2,13 +2,13 @@ import axios from "axios";
 import baseUrl from "./baseUrl";
 
 const fetchSuccess = (payload) => ({ type: "events/fetchSuccess", payload });
-const fetchUserSuccess = (payload) => ({ type: "eventsUser/fetchSuccess", payload })
+const fetchUserSuccess = (payload) => ({ type: "eventsUser/fetchSuccess", payload });
 const fetchDetailSuccess = (payload) => ({ type: "eventDetail/fetchSuccess", payload });
 const addEventSuccess = (payload) => ({ type: "events/addSuccess", payload });
 
 export const fetchEvents = (keyword = "", page) => {
 	return async (dispatch) => {
-		return axios(baseUrl + `/events?page=${page}&size=4&search=${keyword}`).then((payload) => {
+		return axios(baseUrl + `/events/admin?page=${page}&size=4&search=${keyword}`).then((payload) => {
 			dispatch(fetchSuccess(payload.data));
 		});
 	};
@@ -16,7 +16,9 @@ export const fetchEvents = (keyword = "", page) => {
 
 export const fetchYourEvents = (keyword = "", page) => {
 	return async (dispatch) => {
-		return axios(baseUrl + `/events/forUser?page=${page}&size=4&search=${keyword}`, { headers: { access_token: localStorage.access_token } }).then((payload) => {
+		return axios(baseUrl + `/events/forUser?page=${page}&size=4&search=${keyword}`, {
+			headers: { access_token: localStorage.access_token },
+		}).then((payload) => {
 			dispatch(fetchUserSuccess(payload.data));
 		});
 	};
--- server/routers/eventRoute.js
+++ server/routers/eventRoute.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const Controller = require("../controllers/eventController");
 const authentication = require("../middlewares/authentication");
+const Authorization = require("../middlewares/authorization");
 
 router.get("/", Controller.showAllEvent);
-router.get('/forUser', authentication, Controller.showAllEventForUser)
+router.get("/admin", authentication, Controller.showAllEvent);
+router.get("/forUser", authentication, Controller.showAllEventForUser);
 router.post("/add", authentication, Controller.addEvent);
 router.get("/:id", Controller.showEventDetail);
 router.put("/:id/bracket", authentication, Controller.editBracket);
