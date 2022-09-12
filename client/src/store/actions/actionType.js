54ae29147622940684c1979cb934c7c3fd25c59a
steven-leonardo <stevenleonardolim@gmail.com>
2022-09-12 23:34:03 +0700

    modal only for edit team

--- client/src/store/actions/actionType.js
+++ client/src/store/actions/actionType.js
@@ -4,6 +4,7 @@ export const FETCH_ALL_EVENT = 'fetchevent/success';
 export const FETCH_ALL_GAME = 'fetchgame/success';
 export const FETCH_ALL_USER = 'fetchuser/success';
 
+export const FETCH_DETAIL_TEAM = 'fetchdetailteam/success';
 export const FETCH_DETAIL_EVENT = 'fetchdetailevent/success';
 export const FETCH_DETAIL_GAME = 'fetchdetailgame/success';
 export const FETCH_DETAIL_USER = 'fetchdetailuser/success';
