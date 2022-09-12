71b5b8ce413eee12f119805930606a4ac06306d3
Yohanes Kristian Wibowo <yohkristianwibowo@gmail.com>
2022-09-12 12:51:59 +0700

    feat: history list

---
+++ client/src/components/HistoryList.js
@@ -0,0 +1,27 @@
+export default function HistoryList() {
+    return (
+        <>
+            <div style={{ marginTop: "10vh" }} className="text-center">
+                <h1>History List</h1>
+                <table className="table table-striped">
+                    <thead>
+                        <tr style={{ backgroundColor: "#EAE3D2" }}>
+                            <th scope="col">No.</th>
+                            <th scope="col">Team Name</th>
+                            <th scope="col">Event Name</th>
+                            <th scope="col">Payment Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>username</td>
+                            <td>email</td>
+                            <td>tanggal</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </>
+    )
+}
