5e3c8bd5c7dad1a5acfa76f5c0deac68ad090de2
Dhon23 <m.ramadhan.ak@gmail.com>
2022-09-13 09:19:42 +0700

    styling header

--- client/src/components/HeaderBar.js
+++ client/src/components/HeaderBar.js
@@ -1,5 +1,5 @@
 import logo from "../logo.png";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import VerticalModalEditPasswordUser from "./VerticalModalEditPasswordUser";
@@ -31,15 +31,21 @@ export default function HeaderBar() {
         {access_token && <NavLink to="/history-list">History Event</NavLink>}
         {access_token ? (
           <>
-            <i className="fa-solid fa-user" onClick={() => { setModalShow(true); }}></i>
+          <div className="dropdown">
+            <i className="fa-solid fa-user"></i>
+            <div className="submenu">
+              <span onClick={() => { setModalShow(true); }}>Change Password</span>
+              <Link onClick={handleLogout} to="/login">Logout</Link>
+            </div>
+          </div>
             <VerticalModalEditPasswordUser
               show={modalShow}
               onHide={() => setModalShow(false)}
               username={username}
             />
-            <NavLink onClick={handleLogout} to="/login">
+            {/* <NavLink onClick={handleLogout} to="/login">
               Logout
-            </NavLink>
+            </NavLink> */}
           </>
         ) : (
           <NavLink to="/login">Sign In</NavLink>
