bee8a80df75febd40dc28515f978f76248e2c463
Yohanes Kristian Wibowo <yohkristianwibowo@gmail.com>
2022-09-13 12:24:38 +0700

    feat: midtrans automatic update

--- server/routers/midtransRoute.js
+++ server/routers/midtransRoute.js
@@ -2,10 +2,9 @@ const MidtransController = require('../controllers/midtransControllers');
 const authentication = require('../middlewares/authentication');
 const router = require('express').Router();
 
-router.use(authentication);
 // Snap Midtrans
-router.post('/snap-token', MidtransController.generateSnapToken)
+router.post('/snap-token', authentication, MidtransController.generateSnapToken)
 
-// router.patch('/change-status-to-payed', MidtransController.updateStatusToPayed)
+router.post('/change-status-to-payed', MidtransController.updateStatusToPayed)
 
 module.exports = router;
